72876e7e15637a34fca4ed1244de94ea16040dc2
badari <badari0793@gmail.com>
2019-05-02 11:54:36 +0530

    updated version

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -6,7 +6,7 @@ export default () => {
       <h1 className="display-4">
         <span className="text-danger">404</span> Page Not Found
       </h1>
-      <p className="lead">Sorry, that page does not exist</p>
+      <p className="lead">Sorry,that page does not exist</p>
     </div>
   );
 };
